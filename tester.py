37eb2338727f5e9af05f0442c1dc9966734b85f6
stuartki <42723632+stuartki@users.noreply.github.com>
2018-08-26 18:42:14 -0400

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,15 @@
+#writer
+#what if we parse the description for keywords?
+
+import os
+import sys
+from Node import Node
+from writerGrouper import writer
+from readGrouper import reader
+from axiomer import axiomer
+from viewerGrouper import viewer
+from algorithms import reAxiomize
+from editGrouper import edit
+
+topic = "stats250"
+writer(topic)
