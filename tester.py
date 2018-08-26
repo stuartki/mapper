810b3fbf3edddc43c73eb3ad74a127297d7b5700
stuartki <42723632+stuartki@users.noreply.github.com>
2018-08-26 18:43:00 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -6,10 +6,8 @@ import sys
 from Node import Node
 from writerGrouper import writer
 from readGrouper import reader
-from axiomer import axiomer
 from viewerGrouper import viewer
-from algorithms import reAxiomize
 from editGrouper import edit
 
-topic = "stats250"
+topic = ""
 writer(topic)
